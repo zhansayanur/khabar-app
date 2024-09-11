872871f330ba2f1c88ae9edd3f96ad8fdae5259a
Zhansaya <zhansayanur19999@gmail.com>
2024-09-11 11:07:44 +0500

    added inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({
+  subsets: ['latin']
 });
 
 export const metadata: Metadata = {
@@ -25,9 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={inter.className}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <p className="text-3xl font-bold text-indigo-500">
-      Hello world
-    </p>
+    <div className="flex flex-col">
+      <p className="text-3xl font-bold text-indigo-500">
+        Hello world
+      </p>
+      <Button>Click me!</Button>
+    </div>
   );
 }
+
